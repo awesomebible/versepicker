f257a38f52e55a84718d67112355ad9b8adae9dd
Benjamin <benjamin@awesomebible.de>
2023-07-19 15:00:36 +0200

    Update image cdn domain & update caching rule

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,7 +5,7 @@
     <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5 pb-2">
     <!--Card 1-->
     <div class="rounded overflow-hidden shadow-lg dark:bg-blue-950 dark:text-white">
-      <v-lazy-image class="w-full" v-bind:src="'https://images.weserv.nl/?url=verse.awesomebible.de/img/' + year + '/' + day + '.jpg&maxage=1d&w=640'" loading="lazy" alt="Versbild"></v-lazy-image>
+      <v-lazy-image class="w-full" v-bind:src="'https://wsrv.nl/?url=verse.awesomebible.de/img/' + year + '/' + day + '.jpg&w=640'" loading="lazy" alt="Versbild"></v-lazy-image>
       <div class="px-6 py-4 pb-2">
         <div class="font-bold text-xl mb-1">Versbild {{year}} - {{day}}</div>
       </div>
