a051a2b7d798ea0a46c321c1771781853d6b48bd
Benjamin <benjamin@awesomebible.de>
2023-08-13 21:08:38 +0200

    Button-Label angepasst

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
         2022
       </router-link>
       <router-link to="/2021" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-500 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        2023
+        2021
       </router-link>
     </div>
     <Card v-bind:year="year" v-bind:day="day" v-for='day in 366' :key='day'/>
